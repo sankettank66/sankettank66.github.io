cadbfe560edf07767d41b41ea795e4397bca0d52
Sanket Tank <sanket.tank@lucentinnovations.com>
2024-07-24 23:04:56 +0530

    Minor Bug Fixes

--- app/components/CustomizedButton.tsx
+++ app/components/CustomizedButton.tsx
@@ -1,12 +1,19 @@
-import clsx from 'clsx'
-import React from 'react'
+import clsx from 'clsx';
+import React from 'react';
 
 const CustomizedButton = ({ text, className }: { text: string, className: string }) => {
     return (
-        <div className={clsx("px-8 text-center py-0.5 border-2 border-black dark:border-black uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]", className)}>
+        <div
+            className={clsx(
+                "px-8 text-center py-0.5 border-2 border-black dark:border-black uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]",
+                "hover:bg-black hover:text-white dark:hover:bg-black dark:hover:text-white",
+                // Add additional classes from props
+                className
+            )}
+        >
             {text}
         </div>
-    )
-}
+    );
+};
 
-export default CustomizedButton
+export default CustomizedButton;
