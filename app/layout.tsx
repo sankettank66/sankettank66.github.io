f2d5431890cda9c8f86924cc5f03dabde48e102e
Sanket Tank <81406103+sankettank66@users.noreply.github.com>
2024-08-23 17:31:53 +0000

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en" className="dark scroll-smooth">
       <body className={clsx(inter.className, 'scrollbar-thumb-rounded-full', 'scrollbar-track-rounded-full', 'scrollbar', 'scrollbar-thumb-slate-700', 'scrollbar-track-slate-300', 'h-32', 'overflow-y-scroll','w-full')}>
         {children}
-<script src="https://chatbot-ai-support.s3.us-east-2.amazonaws.com/chatbot-main.js" defer="defer"></script>
+<script src="https://chatbot-ai-support.s3.us-east-2.amazonaws.com/chatbot-main.js" defer />
       </body>
     </html>
   );
