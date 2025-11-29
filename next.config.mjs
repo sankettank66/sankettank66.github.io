6cc57aa5bdfe046efba9527b65308ffce0af0cea
Sanket Tank <81406103+sankettank66@users.noreply.github.com>
2025-11-30 01:51:21 +0530

    updated image protocol in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-        ],
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'apps.odoocdn.com',      // <-- THIS is required for your Odoo image
+        pathname: '/**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
